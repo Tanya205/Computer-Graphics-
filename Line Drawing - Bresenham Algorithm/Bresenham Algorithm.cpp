29eb37c71882bef3b827da8acc7844aa95e4161b
tanya205 <tanyamalhotra489@gmail.com>
2021-09-12 07:21:33 +0530

    Bresenham's Algo added

---
+++ Line Drawing - Bresenham Algorithm/Bresenham Algorithm.cpp
@@ -0,0 +1,76 @@
+// Bresenham Line Drawing Algorithm
+#include<stdio.h>
+#include <gl/glut.h>
+#include <Windows.h>
+
+int x1, x2, y1, y2; //Declaring the starting and ending points 
+void myInit()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0, 500, 0, 500);
+}
+
+void draw_pixel(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
+
+void DrawLine(int x1, int x2, int y1, int y2) {
+	int dx, dy;
+	//Step 1 - Calculating dx and dy
+	dx = x2 - x1;
+	dy = y2 - y1;
+	//Step 2 - Calculating the decision parameter
+	int P;
+	P = 2 * dy - dx;
+	//Step 3 - Finding next point with the help of decision parameter
+	int inc_x, inc_y, inc_P;
+	if (P < 0) {
+		inc_P = 2 * dy;
+		inc_x = 1;
+		inc_y = 0;
+	}
+	else if (P >= 0) {
+		inc_P = (2 * dy) - (2 * dx);
+		inc_x = 1;
+		inc_y = 1;
+	}
+	else {
+		return;
+	}
+	//Step 4 - Looping until iteration equals dx - 1 times
+	for (int i = 1; i <= dx - 1; i++) {
+		x1 = x1 + inc_x;
+		y1 = y1 + inc_y;
+		P = P + inc_P;
+		draw_pixel(x1, y1);
+	}
+}
+
+void myDisplay()
+{
+	DrawLine(x1, x2, y1, y2);
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	printf("Enter (x1, y1, x2, y2)\n");
+	scanf_s("%d %d %d %d", &x1, &y1, &x2, &y2);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Bresenham's Line Drawing");
+	myInit();
+	glutDisplayFunc(myDisplay);
+	glutMainLoop();
+
+	return 0;
+}
+
+//x1 = 100, y1 = 100, x2 = 400, y2 = 400 
